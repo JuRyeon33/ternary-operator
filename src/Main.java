8783391819da2634460b0b067e42cafdc2bdb5b4
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-05 11:15:12 +0900

    삼항연산자 score if문으로 변경해보기

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
         int score = 61;
 
-        String result = (score >= 60) ? "통과" : "탈락";
+        String result;
 
-        System.out.println("결과 : " + score);
+        if (score >= 60) {
+            result = "통과";
+        } else {
+            result = "탈락";
+        }
+        System.out.println("결과 : " + result);
     }
 }
